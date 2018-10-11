a45b114c1948d34d6b16a02fa70c502f49a8647b
Jesse Alama <jesse.alama@vicampo.de>
2018-10-11 14:50:08 +0200

    Dump unused stuff

--- parser-test.rkt
+++
@@ -1,8 +0,0 @@
-#lang br/quicklang
-
-(require (file "grammar.rkt")
-         (file "tokenizer.rkt"))
-
-(module+ main
-  (syntax->datum
-   (parse (tokenize-file "/Users/jessealama/src/vicampo/platform/tests/riposte/vip-2962.rip"))))
