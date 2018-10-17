ca3435ed53f2305b211b83c5f5ee912aa301d04c
Jesse Alama <jesse.alama@vicampo.de>
2018-10-17 16:34:07 +0200

    Remove support for + mixing strings and integers
    I must have been confusing + with *.

--- arithmetic.rkt
+++ arithmetic.rkt
@@ -16,23 +16,6 @@
 (module+ test
   (require rackunit))
 
-(define/contract (duplicate-string str times)
-  (string? exact-nonnegative-integer? . -> . string?)
-  (cond [(= 0 times)
-         ""]
-        [(= 1 times)
-         str]
-        [else
-         (format "~a~a" str (duplicate-string str (sub1 times)))]))
-
-(module+ test
-  (check-equal? ""
-                (duplicate-string "french fries & gravy" 0))
-  (check-equal? "french fries"
-                (duplicate-string "french fries" 1))
-  (check-equal? "abcabc"
-                (duplicate-string "abc" 2)))
-
 (define sum%
   (class expression%
     (super-new)
@@ -43,50 +26,20 @@
       (cond [(real? l)
              (cond [(real? r)
                     (+ l r)]
-                   [(string? r)
-                    (unless (positive? l)
-                      (cond [(expression? l)
-                             (error (format "Cannot add: ~a works out to ~a, which is not positive."
-                                            (send lhs render)
-                                            (render-ejsexprish l)))]
-                            [else
-                             (error (format "Cannot add: ~a is not positive." l))]))
-                    (unless (exact-nonnegative-integer? l)
-                      (cond [(expression? l)
-                             (error (format "Cannot add: ~a works out to ~a, which is not an integer."
-                                            (send lhs render)
-                                            (render-ejsexprish l)))]
-                            [else
-                             (error (format "Cannot add: ~a is not an integer." l))]))
-                    (duplicate-string r l)]
                    [else
-                    (cond [(expression? l)
-                           (error (format "Cannot add: ~a works out to ~a, which is neither a string nor an integer."
-                                          (send lhs render)
-                                          (render-ejsexprish l)))]
-                          [else
-                           (error (format "Cannot add: ~a is neither a string nor an integer"
-                                          (render-ejsexprish l)))])])]
+                    (error "Cannot add: the left-hand side is a number, but the right-hand side isn't.")])]
             [(string? l)
-             (cond [(exact-nonnegative-integer? r)
-                    (duplicate-string l r)]
-                   [(string? r)
+             (cond [(string? r)
                     (format "~a~a" l r)]
                    [else
-                    (cond [(expression? r)
-                           (error (format "Cannot add: ~a works out to ~a, which is neither a string nor an integer."
-                                          (send rhs render)
-                                          (render-ejsexprish r)))]
-                          [else
-                           (error (format "Cannot add: ~a is neither a string nor an integer"
-                                          (render-ejsexprish r)))])])]
+                    (error "Cannot add: the left-hand side is a number, but the right-hand side isn't.")])]
             [else
              (cond [(expression? l)
-                    (error (format "Cannot add: ~a works out to ~a, which is neither a string nor an integer."
+                    (error (format "Cannot add: ~a works out to ~a, which is neither a string nor a number."
                                    (send lhs render)
                                    (render-ejsexprish l)))]
                    [else
-                    (error (format "Cannot add: ~a is neither a string nor an integer"
+                    (error (format "Cannot add: ~a is neither a string nor a number."
                                    (render-ejsexprish l)))])]))
     (define/override (render)
       (define l (render-ejsexprish lhs))
