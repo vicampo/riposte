7d216ec2dfd314ad13aa49cd23e54561e685445a
Jesse Alama <jesse.alama@vicampo.de>
2018-11-30 13:14:23 +0100

    Support the #:version keyword

--- dependencies.rkt
+++ dependencies.rkt
@@ -1,9 +1,15 @@
 #lang racket/base
 
-(require setup/getinfo)
+(require setup/getinfo
+         racket/match)
 
 (define deps (get-info/full (current-directory)))
 
 (module+ main
   (for [(d (deps 'deps))]
-    (displayln d)))
+    (displayln
+     (match d
+       [(? string?)
+        d]
+       [(list (? string? p) #:version whatever)
+        p]))))
