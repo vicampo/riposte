d622a37213a7eefa29e180ef3d7dcb9e08a9e381
Jesse Alama <jesse@lisp.sh>
2019-08-19 07:57:21 +0200

    Supply the reader directly

--- main.rkt
+++ main.rkt
@@ -1,5 +1,16 @@
 #lang racket/base
 
 (module reader racket/base
-  (require "reader.rkt")
-  (provide read-syntax))
+  (provide read-syntax)
+
+  (require (only-in (file "tokenizer.rkt")
+                    tokenize)
+           (only-in (file "grammar.rkt")
+                    parse))
+
+  (define (read-syntax path port)
+    (define parse-tree (parse path (tokenize port)))
+    (datum->syntax
+     #f
+     `(module riposte-module riposte/expander
+        ,parse-tree))))
