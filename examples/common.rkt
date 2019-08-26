c8eb1f4cb29f99b07d88a15f97b7ff4efa47042d
Jesse Alama <jesse@lisp.sh>
2019-08-26 07:44:51 +0200

    Add more common infrastructure

--- examples/common.rkt
+++ examples/common.rkt
@@ -1,14 +1,15 @@
 #lang racket/base
 
+(provide run)
+
 (require racket/list
          racket/contract
          json
-         (except-in net/cookies/server
-                    make-cookie)
-         (file "request-structs.rkt")
-         (file "response-structs.rkt")
-         (file "cookie.rkt")
-         (file "status-code.rkt"))
+         web-server/http/response-structs
+         web-server/http/request-structs
+         web-server/http/cookie
+         web-server/servlet-env
+         net/cookie)
 
 (module+ test
   (require rackunit))
@@ -18,20 +19,51 @@
          #:code [code 200]
          #:message [message #f]
          #:seconds [seconds (current-seconds)]
-         #:mime-type [mime-type APPLICATION/JSON-MIME-TYPE]
+         #:mime-type [mime-type #"application/json;charset=utf-8"]
          #:cookies [cooks empty]
          #:headers [hdrs empty])
-  (define js-null (json-null))
-  (response
-   code (infer-response-message code message) seconds mime-type
-   ; rfc2109 also recommends some cache-control stuff here for cookies
+  (response/full
+   code
+   message
+   seconds
+   mime-type
    (append hdrs (map cookie->header cooks))
-   (λ (out)
-     (write-json jsexpr out #:null js-null))))
+   (list (jsexpr->bytes jsexpr #:null (json-null)))))
+
+(define (response/empty
+         #:code [code 200]
+         #:message [message #f]
+         #:seconds [seconds (current-seconds)]
+         #:mime-type [mime-type #""]
+         #:cookies [cooks empty]
+         #:headers [hdrs empty])
+  (response/full
+   code
+   message
+   seconds
+   mime-type
+   (append hdrs (map cookie->header cooks))
+   (list)))
+
+(define (run servlet #:port [port 12345])
+  (serve/servlet servlet
+                 #:port port
+                 #:stateless? #t
+                 #:command-line? #t
+                 #:servlet-regexp (regexp ".*")))
 
 (provide/contract
  [response/jsexpr
   ((jsexpr?)
+   (#:code response-code/c
+    #:message (or/c #f bytes?)
+    #:seconds real?
+    #:mime-type (or/c #f bytes?)
+    #:cookies (listof cookie?)
+    #:headers (listof header?))
+   . ->* . response?)]
+ [response/empty
+  (()
    (#:code response-code/c
     #:message (or/c #f bytes?)
     #:seconds real?
