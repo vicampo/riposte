a3d533918de7e3d87a9e112547bdfef827824575
Jesse Alama <jesse@lisp.sh>
2019-08-26 07:47:18 +0200

    Provide needed bindings

--- main.rkt
+++ main.rkt
@@ -1,16 +1,49 @@
 #lang racket/base
 
+(provide #%module-begin
+         #%app
+         #%top
+         #%datum
+         riposte-program
+         header-assignment
+         parameter-assignment
+         normal-assignment
+         expression
+         json-object
+         json-object-item
+         json-array
+         command
+         uri-template
+         uri-template-expression
+         uri-template-variable-list
+         uri-template-varspec
+         has-type
+         json-pointer)
+
+(require (file "expander.rkt"))
+
 (module reader racket/base
-  (provide read-syntax)
+  (provide (rename-out [riposte:read-syntax read-syntax]
+                       [riposte:read read])
+           get-info)
 
-  (require (only-in (file "tokenizer.rkt")
-                    tokenize)
-           (only-in (file "grammar.rkt")
-                    parse))
+  (require (only-in (file "grammar.rkt")
+                    parse)
+           (only-in (file "tokenizer.rkt")
+                    tokenize))
 
-  (define (read-syntax path port)
-    (define parse-tree (parse path (tokenize port)))
-    (datum->syntax
-     #f
-     `(module riposte-module riposte/expander
-        ,parse-tree))))
+  (define (riposte:read-syntax name in)
+    (define parse-tree (parse name (tokenize in)))
+    (datum->syntax #f `(module anything riposte
+                         ,parse-tree)))
+
+  (define (riposte:read in)
+    (syntax->datum (riposte:read-syntax 'src in)))
+
+  (define (get-info port src-mod src-line src-col src-pos)
+    (define (handle-query key default)
+      (case key
+        [(module-language)
+         "expander.rkt"]
+        [else default]))
+    handle-query))
