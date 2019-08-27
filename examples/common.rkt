801b757f62a23566177118ecb8ec56902c30ca48
Jesse Alama <jesse@lisp.sh>
2019-08-27 08:14:14 +0200

    Extend base functionality

--- examples/common.rkt
+++ examples/common.rkt
@@ -45,6 +45,22 @@
    (append hdrs (map cookie->header cooks))
    (list)))
 
+(define (not-found req)
+  (response/empty #:code 404))
+
+(define (not-allowed req)
+  (response/empty #:code 405))
+
+(define content-type-regexp
+  (byte-regexp "[Cc][Oo][Nn][Tt][Ee][Nn][Tt][-][Tt][Yy][Pp][Ee]"))
+
+(define (has-json-mime-header req)
+  (match (headers-assq* #"Content-Type" (request-headers/raw req))
+    [#f
+     #f]
+    [(? header? h)
+     (regexp-match? content-type-regexp (header-value h))]))
+
 (define (run servlet #:port [port 12345])
   (serve/servlet servlet
                  #:port port
@@ -70,4 +86,6 @@
     #:mime-type (or/c #f bytes?)
     #:cookies (listof cookie?)
     #:headers (listof header?))
-   . ->* . response?)])
+   . ->* . response?)]
+ [not-found (request? . -> . response?)]
+ [not-allowed (request? . -> . response?)])
