317ddf28842cf331f031be296bc568018e88be5c
Jesse Alama <jesse@lisp.sh>
2019-08-31 22:02:31 +0200

    Define JSON arrays

--- json.rkt
+++ json.rkt
@@ -1,9 +1,14 @@
 #lang racket/base
 
-(provide json-object?)
+(provide json-object?
+         json-array?)
 
 (require json)
 
+; it is assumed that x is a jsexpr? value
 (define (json-object? x)
-  (and (jsexpr? x)
-       (hash? x)))
+  (hash? x))
+
+; it is assumed that x is a jsexpr? value
+(define (json-array? x)
+  (list? x))
