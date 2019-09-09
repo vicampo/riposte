0658d9bb86a54f64045e08659bf582d486889645
Jesse Alama <jesse.alama@vicampo.de>
2019-09-09 14:38:22 +0200

    Tweak example

--- examples/empty.rkt
+++ examples/empty.rkt
@@ -6,42 +6,36 @@
          json
          (file "common.rkt"))
 
-(define (view-cart req cart-id)
-  (response/jsexpr (hasheq 'grand_total 34.58)
-                   #:code 200))
+(define baz 42)
 
-(define (add-item-to-cart req cart-id)
-  (match (request-post-data/raw req)
-    [#f
-     (response/empty #:code 400)]
-    [(? bytes? bs)
-     (with-handlers ([exn:fail? (lambda (err)
-                                  (response/empty #:code 400))])
-       (match (bytes->jsexpr bs)
-         [(hash-table ('product_id (? exact-positive-integer? product-id))
-                      ('campaign_id (? exact-positive-integer? campaign-id))
-                      ('qty (? exact-positive-integer? qty)))
-          (define item (hasheq 'product_id product-id
-                               'campaign_id campaign-id
-                               'qty qty
-                               'cart_item_id 42))
-          (define price (* qty (+ product-id campaign-id (random 1 25))))
-          (response/jsexpr (hasheq 'grand_total price
-                                   'items (list item))
-                           #:code 200)]
-         [else
-          (response/empty #:code 400)]))]))
+(define bomb -59.8)
 
 (define (robby req)
   (response/jsexpr (hasheq 'foo "wow!"
                            'bar (list "foo")
-                           'baz 42
-                           'bomb -59.8)))
+                           'baz baz
+                           'bomb bomb)))
+
+(define (jay req)
+  (define body (request->jsexpr req))
+  (match (request->jsexpr req)
+    [(hash-table ('sum s)
+                 ('product p)
+                 ('difference d))
+     (cond [(and (= s (+ baz bomb))
+                 (= p (* baz bomb))
+                 (= d (- baz bomb)))
+            (response/empty #:code 201
+                            #:headers (list (make-header #"Location" #"/jay/zee")))])]
+    [else
+     (response/empty #:code 400)]))
 
 (define-values (start url-generator)
   (dispatch-rules
    [("robby") #:method "get" robby]
-   [("cart" (string-arg) "items") #:method "post" add-item-to-cart]))
+   [("robby") #:method (regexp ".*") not-allowed]
+   [("jay") #:method "post" jay]
+   [("jay") #:method (regexp ".*") not-allowed]))
 
 (module+ main
   (run start))
