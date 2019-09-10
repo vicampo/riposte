fc5d937ed93749c0bf7811ca23e1b9d927c44f2c
Jesse Alama <jesse.alama@vicampo.de>
2019-09-10 15:23:09 +0200

    Add new password script for exec example

---
+++ examples/password.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+secret_password='SECRET!'
+opt_timestamp=''
+opt_password=''
+opt_quiet=''
+now=$(date -u)
+who=$(whoami)
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+	-q|--quiet)
+	    opt_quiet='y'
+	    shift
+	    ;;
+	-t|--timestamp)
+	    opt_timestamp='y'
+	    shift
+	    ;;
+	-p|--password)
+	    opt_password='y'
+	    shift
+	    ;;
+	*)
+	    echo "Unknown option '${key}'."
+	    exit 1
+	    ;;
+    esac
+done
+
+# running quietly means not doing anything
+if [ "${opt_quiet}" = "y" ]; then
+    exit 0;
+fi
+
+if [ "${opt_password}" = "y" ]; then
+    if [ "${opt_timestamp}" = "y" ]; then
+	echo $(printf '{"username":"%s","password":"%s","now":"%s"}' "${who}" "${secret_password}" "${now}")
+    else
+	echo $(printf '{"username":"%s","password":"%s"}' "${who}" "${secret_password}")
+    fi
+elif [ "${opt_timestamp}" = "y" ]; then
+    echo $(printf '{"username":"%s","now":"%s"}' "${who}" "${now}")
+else
+    echo $(printf '{"username":"%s"}' "${who}")
+fi
