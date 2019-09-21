43ceb55cd0d7b41e9d52f1a39a423340622b9867
Jesse Alama <jesse@lisp.sh>
2019-09-21 13:19:08 +0200

    Add more to the equals example

--- examples/equals.rkt
+++ examples/equals.rkt
@@ -1,13 +1,32 @@
 #lang racket/base
 
-(require (file "common.rkt"))
+(require racket/match
+         racket/hash
+         (file "common.rkt"))
 
 (define (foo/bar req)
   (response/jsexpr (hash 'hey #f)))
 
+(define (post:foo/bar req)
+  (match (cons (request->jsexpr req)
+               (get-request-header req #"accept"))
+    [(cons (? void?) _)
+     (response/empty #:code 400)]
+    [(cons (not (? hash?)) _ )
+     (response/empty #:code 400)]
+    [(cons js #f)
+     (response/jsexpr (hash-union (hash 'hey #f) (hash 'body js)))]
+    [(cons js (? bytes? h))
+     (match (bytes->string h)
+       [(? string? s)
+        (response/jsexpr (hash-union (hash 'hey #t) (hash 'body js 'content-type s)))]
+       [else
+        (response/empty #:code 400)])]))
+
 (define-values (start url-generator)
   (dispatch-rules
    [("foo" "bar") #:method "get" foo/bar]
+   [("foo" "bar") #:method "post" post:foo/bar]
    [("foo" "bar") #:method (regexp ".*") not-allowed]
    [else not-found]))
 
