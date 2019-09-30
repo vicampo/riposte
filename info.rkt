52589d0a3170ae5f3e5905eb7c70d23361599db3
Jesse Alama <jesse@lisp.sh>
2019-09-30 19:33:20 +0200

    Bump

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "riposte")
 
-(define version "0.12.0")
+(define version "0.12.1")
 
 (define deps
   '("br-parser-tools-lib"
--- version.rkt
+++ version.rkt
@@ -2,4 +2,4 @@
 
 (provide riposte-version)
 
-(define riposte-version "0.12.0")
+(define riposte-version "0.12.1")
