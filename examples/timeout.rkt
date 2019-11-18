e16638f3440ffcc6a9c1313d4532328b29c74cbc
Jesse Alama <jesse@lisp.sh>
2019-11-18 07:37:54 +0100

    Add timeout example

--- examples/timeout.rkt
+++ examples/timeout.rkt
@@ -1,9 +1,46 @@
-#lang racket/base
+#lang racket
 
 (require (file "common.rkt"))
 
-(define (start req)
-  (response/jsexpr (list) #:code 299))
+(define timeouts
+  (hash 'disneyland 10
+        'mcdonalds 1))
+
+(define (slow req)
+  (sleep 60)
+  (response/empty))
+
+(define (join req)
+  (log-error "body: ~a" (request->jsexpr req))
+  (match (request->jsexpr req)
+    [(? hash? h)
+     (match (hash-ref h 'destination #f)
+       [(? string? s)
+        (define dest (string-downcase (string-trim s)))
+        (define host (string->symbol dest))
+        (match (hash-ref timeouts host #f)
+          [#f
+           (response/empty #:code 404)]
+          [(? exact-nonnegative-integer? t)
+           (sleep t)
+           (define header (make-header #"Location"
+                                       (string->bytes/utf-8 dest)))
+           (response/jsexpr (hasheq 'ticket_for dest)
+                            #:code 200
+                            #:headers (list header))]
+          [else
+           (response/empty #:code 500)])]
+       [else
+        (response/empty #:code 400)])]
+    [else
+     (response/jsexpr #:code 400)]))
+
+(define-values (start url-generator)
+  (dispatch-rules
+   [("sloooooooow") #:method "get" slow]
+   [("sloooooooow") #:method (regexp ".*") not-allowed]
+   [("join-queue") #:method "post" join]
+   [("join-queue") #:method (regexp ".*") not-allowed]))
 
 (module+ main
   (run start))
